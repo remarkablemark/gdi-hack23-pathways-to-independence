5a0fa7de4dd2cfb54228139c5483987ca5ae0cd2
Michaela Budde <michaelabudde@Michaelas-MacBook-Pro.local>
2023-11-10 15:59:00 -0600

    dashboard link corrected

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -24,7 +24,7 @@ export default function TabLayout() {
       }}
     >
       <Tabs.Screen
-        name="index"
+        name="home"
         options={{
           title: 'Home',
           tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
