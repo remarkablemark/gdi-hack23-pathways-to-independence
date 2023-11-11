63e86e2722ac988f627c169a91b9704078e4822d
Chastity Sapp <chastity.sapp@gmail.com>
2023-11-11 03:44:18 -0500

    Added Homeowner Course

---
+++ constants/homeownerCourse.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const HomeownerCourse = () => {
+  return (
+    <div>
+      <h1>Homeowner Course</h1>
+      <h2>Introduction</h2>
+      <ul>
+        <li>Welcome to Homeownership</li>
+        <li>The Importance of Being a Homeowner</li>
+        <li>Setting Goals as a Homeowner</li>
+      </ul>
+
+      <h2>Financial Planning for Homeowners</h2>
+      <ul>
+        <li>Budgeting for Homeownership</li>
+        <li>Managing Mortgage Payments</li>
+        <li>Building an Emergency Fund</li>
+      </ul>
+
+      <h2>Home Maintenance and Repairs</h2>
+      <ul>
+        <li>Basic Home Maintenance Tasks</li>
+        <li>Identifying Common Home Repair Issues</li>
+        <li>Hiring Contractors</li>
+      </ul>
+
+      {/* Add more thought-provoking homeowner course content */}
+    </div>
+  );
+};
+
+export default HomeownerCourse;
