2fa64332fefdab77ae41e646f4b05ae4ec534804
Denise <deniselewis1120@gmail.com>
2023-11-10 19:54:27 -0800

    Add menu icon to AppBar

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,44 +1,54 @@
 import { Link } from 'expo-router';
-import { StyleSheet, ScrollView } from 'react-native';
-import { Card, Button, Text } from 'react-native-paper';
+import { StyleSheet, ScrollView, View } from 'react-native';
+import { Card, Button, Text, Appbar } from 'react-native-paper';
 
-import { View } from '../../components/Themed';
+//import { View } from '../../components/Themed';
 
 export default function HomeScreen() {
   return (
-    <ScrollView>
-      <Card mode="outlined">
-        <Card.Cover source={{ uri: require('../../assets/svgs/enroll.svg') }} />
+    <View>
+      <Appbar.Header>
+        <Appbar.Content title="" />
+        <Appbar.Action icon="menu" onPress={() => {}} />
+      </Appbar.Header>
+      <ScrollView>
+        <Card mode="outlined">
+          <Card.Cover
+            source={{ uri: require('../../assets/svgs/enroll.svg') }}
+          />
 
-        <Card.Content>
-          <Text variant="titleLarge"> Enroll </Text>
-          <Text variant="bodyMedium">
-            {' '}
-            Click here to see courses available for you to enroll in.{' '}
-          </Text>
-        </Card.Content>
-        <Card.Actions>
-          <Button>
-            <Link href="/about">Learn More</Link>
-          </Button>
-        </Card.Actions>
-      </Card>
+          <Card.Content>
+            <Text variant="titleLarge"> Enroll </Text>
+            <Text variant="bodyMedium">
+              {' '}
+              Click here to see courses available for you to enroll in.{' '}
+            </Text>
+          </Card.Content>
+          <Card.Actions>
+            <Button>
+              <Link href="/about">Learn More</Link>
+            </Button>
+          </Card.Actions>
+        </Card>
 
-      <Card mode="outlined">
-        <Card.Cover source={{ uri: require('../../assets/svgs/arrows.svg') }} />
+        <Card mode="outlined">
+          <Card.Cover
+            source={{ uri: require('../../assets/svgs/arrows.svg') }}
+          />
 
-        <Card.Content>
-          <Text variant="titleLarge"> Roadmap </Text>
-          <Text variant="bodyMedium">
-            {' '}
-            Click here to see your journey to healing and independence.{' '}
-          </Text>
-        </Card.Content>
-        <Card.Actions>
-          <Button>Learn More</Button>
-        </Card.Actions>
-      </Card>
-    </ScrollView>
+          <Card.Content>
+            <Text variant="titleLarge"> Roadmap </Text>
+            <Text variant="bodyMedium">
+              {' '}
+              Click here to see your journey to healing and independence.{' '}
+            </Text>
+          </Card.Content>
+          <Card.Actions>
+            <Button>Learn More</Button>
+          </Card.Actions>
+        </Card>
+      </ScrollView>
+    </View>
   );
 }
 
