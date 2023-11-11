50a494f4a36d55e89cc24a158a4611b8526ea2fb
Chastity Sapp <chastity.sapp@gmail.com>
2023-11-11 03:33:53 -0500

    Added Dashboard Content

---
+++ constants/dashboardContent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DashboardContent = () => {
+  return (
+    <div>
+      <h1>Dashboard Content</h1>
+      {/* Add your dashboard content here */}
+    </div>
+  );
+};
+
+export default DashboardContent;
