483ea5e812cd5fbe2d1bb7937deefde1a112ffab
Chastity Sapp <chastity.sapp@gmail.com>
2023-11-11 03:20:16 -0500

    Add new images and update styles

---
+++ constants/coursesContent.js
@@ -0,0 +1,18 @@
+// Tab names
+export const HOME_TAB = 'Home';
+export const RESOURCES_TAB = 'Resources';
+export const PROFILE_TAB = 'Profile';
+export const SETTINGS_TAB = 'Settings';
+
+// Home subpages
+export const DASHBOARD_SCREEN = 'Dashboard';
+export const NOTIFICATIONS_SCREEN = 'Notifications';
+
+// Resources subpages
+export const JOB_ASSISTANCE_SCREEN = 'JobAssistance';
+export const EDUCATIONAL_RESOURCES_SCREEN = 'EducationalResources';
+export const FINANCIAL_GUIDANCE_SCREEN = 'FinancialGuidance';
+
+// Profile subpages
+export const PERSONAL_INFORMATION_SCREEN = 'PersonalInformation';
+export const ACHIEVEMENTS_SCREEN = 'Achievements';
